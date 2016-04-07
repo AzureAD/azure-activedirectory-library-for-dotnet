115861b0d20f8b7e5c12c2ea3b2ee6b1595abb69
Espen Tveit <espen.tveit@cegal.com>
2016-04-07 15:28:52 +0200

    Add support for AcquireToken(string resource, string clientId, string clientSecret, UserCredential userCredential)

--- src/ADAL.Common/AcquireTokenNonInteractiveHandler.cs
+++ src/ADAL.Common/AcquireTokenNonInteractiveHandler.cs
@@ -25,6 +25,7 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
     internal class AcquireTokenNonInteractiveHandler : AcquireTokenHandlerBase
     {
         private readonly UserCredential userCredential;
+        private readonly string clientSecret;
         private UserAssertion userAssertion;
         
         public AcquireTokenNonInteractiveHandler(Authenticator authenticator, TokenCache tokenCache, string resource, string clientId, UserCredential userCredential, bool callSync)
@@ -36,6 +37,17 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
             }
 
             this.userCredential = userCredential;
+        }
+
+        public AcquireTokenNonInteractiveHandler(Authenticator authenticator, TokenCache tokenCache, string resource, string clientId, string clientSecret, UserCredential userCredential, bool callSync)
+            : base(authenticator, tokenCache, resource, new ClientKey(clientId), TokenSubjectType.User, callSync)
+        {
+            if (userCredential == null)
+            {
+                throw new ArgumentNullException("userCredential");
+            }
+            this.userCredential = userCredential;
+            this.clientSecret = clientSecret;
         }
 
         public AcquireTokenNonInteractiveHandler(Authenticator authenticator, TokenCache tokenCache, string resource, string clientId, UserAssertion userAssertion, bool callSync)
@@ -128,6 +140,11 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
 
         protected override void AddAditionalRequestParameters(RequestParameters requestParameters)
         {
+            if(this.clientSecret != null)
+            {
+                requestParameters[OAuthParameter.ClientSecret] = this.clientSecret;
+            }
+
             if (this.userAssertion != null)
             {
                 requestParameters[OAuthParameter.GrantType] = this.userAssertion.AssertionType;
