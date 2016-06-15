d1998d135781f13125ecacf452533109f46aa5bb
Kanishk Panwar <kpanwar@microsoft.com>
2016-06-15 13:54:42 -0700

    Fix to allow hardware based keys to perform authentication. We should only up-level software keys using the CAPI PROV_RSA_FULL and PROV_RSA_SCHANNEL providers and never up-level hardware based keys.

--- src/ADAL.NET/CryptographyHelper.cs
+++ src/ADAL.NET/CryptographyHelper.cs
@@ -29,7 +29,8 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
         public static byte[] SignWithCertificate(string message, X509Certificate2 x509Certificate)
         {
             X509AsymmetricSecurityKey x509Key = new X509AsymmetricSecurityKey(x509Certificate);
-            RSACryptoServiceProvider rsa = x509Key.GetAsymmetricAlgorithm(SecurityAlgorithms.RsaSha256Signature, true) as RSACryptoServiceProvider;
+            RSACryptoServiceProvider rsa =
+                x509Key.GetAsymmetricAlgorithm(SecurityAlgorithms.RsaSha256Signature, true) as RSACryptoServiceProvider;
 
             RSACryptoServiceProvider newRsa = null;
             try
@@ -62,30 +63,32 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
         // This method returns an AsymmetricSignatureFormatter capable of supporting Sha256 signatures. 
         private static RSACryptoServiceProvider GetCryptoProviderForSha256(RSACryptoServiceProvider rsaProvider)
         {
-            const int PROV_RSA_AES = 24;    // CryptoApi provider type for an RSA provider supporting sha-256 digital signatures
-            if (rsaProvider.CspKeyContainerInfo.ProviderType == PROV_RSA_AES)
+            const int PROV_RSA_AES = 24;
+                // CryptoApi provider type for an RSA provider supporting sha-256 digital signatures
+            if ((rsaProvider.CspKeyContainerInfo.ProviderType == 1 || rsaProvider.CspKeyContainerInfo.ProviderType == 12) &&
+                !rsaProvider.CspKeyContainerInfo.HardwareDevice)
             {
-                return rsaProvider;
+                CspParameters csp = new CspParameters
+                {
+                    ProviderType = PROV_RSA_AES,
+                    KeyContainerName = rsaProvider.CspKeyContainerInfo.KeyContainerName,
+                    KeyNumber = (int) rsaProvider.CspKeyContainerInfo.KeyNumber
+                };
+
+                if (rsaProvider.CspKeyContainerInfo.MachineKeyStore)
+                {
+                    csp.Flags = CspProviderFlags.UseMachineKeyStore;
+                }
+
+                //
+                // If UseExistingKey is not specified, the CLR will generate a key for a non-existent group.
+                // With this flag, a CryptographicException is thrown instead.
+                //
+                csp.Flags |= CspProviderFlags.UseExistingKey;
+                return new RSACryptoServiceProvider(csp);
             }
 
-            CspParameters csp = new CspParameters
-                                {
-                                    ProviderType = PROV_RSA_AES,
-                                    KeyContainerName = rsaProvider.CspKeyContainerInfo.KeyContainerName,
-                                    KeyNumber = (int)rsaProvider.CspKeyContainerInfo.KeyNumber
-                                };
-
-            if (rsaProvider.CspKeyContainerInfo.MachineKeyStore)
-            {
-                csp.Flags = CspProviderFlags.UseMachineKeyStore;
-            }
-
-            //
-            // If UseExistingKey is not specified, the CLR will generate a key for a non-existent group.
-            // With this flag, a CryptographicException is thrown instead.
-            //
-            csp.Flags |= CspProviderFlags.UseExistingKey;
-            return new RSACryptoServiceProvider(csp);
+            return rsaProvider;
         }
     }
 }
