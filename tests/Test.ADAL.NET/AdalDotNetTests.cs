af5b8dd9364bc6b9b58c89fe7ab15d318281f7cf
Kanishk Panwar <kpanwar@microsoft.com>
2016-07-05 16:19:17 -0700

    Unexpected multiple_matching_tokens_detected even when User has been provided. The problem occurs because AcquireTokenHandlerBase initializes CacheQueryData in its constructor and it uses fields such as this.UniqueId and this.DisplayableId that have not been initialized yet. They get initialized by subclasses such as AcquireTokenSilentHandler or AcquireTokenInteractiveHandler after the base class constructor exits.
    Thus, search by user id or displayable name never works and we get this error about duplicates.

--- tests/Test.ADAL.NET/AdalDotNetTests.cs
+++ tests/Test.ADAL.NET/AdalDotNetTests.cs
@@ -19,6 +19,7 @@
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Test.ADAL.Common;
 using Test.ADAL.NET.Friend;
