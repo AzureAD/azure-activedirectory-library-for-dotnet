c7def5ee65dbf78f5923666ef4d8e5f96c4ca429
abhishek58g <abgun@microsoft.com>
2016-11-15 19:26:19 -0800

    Adding Cache Operations to the TestApp

--- automation/WinFormsAutomationApp/AuthenticationHelper.cs
+++ automation/WinFormsAutomationApp/AuthenticationHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,10 +46,78 @@ namespace WinFormsAutomationApp
             return output;
         }
         
-        public static Task<string> ExpireAccessToken(Dictionary<string, string> input)
+        public static async Task<string> ExpireAccessToken(Dictionary<string, string> input)
         {
-            return null;
-            //TokenCache.DefaultShared.
+            Task<string> myTask = Task<string>.Factory.StartNew(() =>
+            {
+            List<KeyValuePair<TokenCacheKey, AuthenticationResultEx>> CacheItems = QueryCache(input["authority"],
+                input["client_id"], input["unique_id"], input["displayable_id"],
+                input["assertion_hash"]);
+
+            foreach (KeyValuePair<TokenCacheKey, AuthenticationResultEx> item in CacheItems)
+            {
+                item.Value.Result.ExpiresOn = DateTime.UtcNow;
+                TokenCache.DefaultShared.tokenCacheDictionary[item.Key] = item.Value;
+            }
+            Dictionary<string, string> output = new Dictionary<string, string>();
+                //Send back error if userId or displayableId is not sent back to the user
+            output.Add("expired_access_token_count", CacheItems.Count.ToString());
+            return output.ToJson();
+            });
+
+            return await myTask;
+        }
+
+        public static async Task<string> InvalidateRefreshTokens(Dictionary<string, string> input)
+        {
+            Task<string> myTask = Task<string>.Factory.StartNew(() =>
+            {
+                List<KeyValuePair<TokenCacheKey, AuthenticationResultEx>> CacheItems = QueryCache(input["authority"],
+                input["client_id"], input["unique_id"], input["displayable_id"],
+                input["assertion_hash"]);
+
+            foreach (KeyValuePair<TokenCacheKey, AuthenticationResultEx> item in CacheItems)
+            {
+                item.Value.RefreshToken = "bad_refresh_token";
+                TokenCache.DefaultShared.tokenCacheDictionary[item.Key] = item.Value;
+            }
+                Dictionary<string, string> output = new Dictionary<string, string>();
+                //Send back error if userId or displayableId is not sent back to the user
+                output.Add("invalidated_refresh_token_count", CacheItems.Count.ToString());
+                return output.ToJson();
+            });
+
+            return await myTask;
+        }
+
+        public static async Task<string> ReadCache(Dictionary<string, string> input)
+        {
+            Task<string> myTask = Task<string>.Factory.StartNew(() =>
+            {
+                int count = TokenCache.DefaultShared.Count;
+                Dictionary<string, string> output = new Dictionary<string, string>();
+                output.Add("item_count", count.ToString());
+                var list = TokenCache.DefaultShared.ReadItems().ToJson();
+                output.Add("AccessToken", list);
+                return output.ToJson();
+            });
+
+            return await myTask;
+        }
+
+        public static async Task<string> ClearCache()
+        {
+            Task<string> myTask = Task<string>.Factory.StartNew(() =>
+            {
+                int count = TokenCache.DefaultShared.Count;
+                Dictionary<string, string> output = new Dictionary<string, string>();
+                output.Add("item_count", count.ToString());
+                TokenCache.DefaultShared.Clear();
+                output.Add("cache_clear_status", "Cleared the entire cache");
+                return output.ToJson();
+            });
+
+            return await myTask;
         }
 
         public static string ToJson(this object obj)
@@ -71,6 +140,20 @@ namespace WinFormsAutomationApp
         {
             var jss = new JavaScriptSerializer();
             return jss.Deserialize<Dictionary<string, string>>(json); 
+        }
+
+        private static List<KeyValuePair<TokenCacheKey, AuthenticationResultEx>> QueryCache(string authority,
+            string clientId, string uniqueId, string displayableId,
+            string assertionHash)
+        {
+            return TokenCache.DefaultShared.tokenCacheDictionary.Where(
+                    p =>
+                        (string.IsNullOrWhiteSpace(authority) || p.Key.Authority == authority)
+                        && (string.IsNullOrWhiteSpace(clientId) || p.Key.ClientIdEquals(clientId))
+                        && (string.IsNullOrWhiteSpace(uniqueId) || p.Key.UniqueId == uniqueId)
+                        && (string.IsNullOrWhiteSpace(displayableId) || p.Key.DisplayableIdEquals(displayableId))
+                        && (string.IsNullOrWhiteSpace(assertionHash) || assertionHash.Equals(p.Value.UserAssertionHash)))
+                    .ToList();
         }
 
         private static string JsonOutputFormat(string result)
