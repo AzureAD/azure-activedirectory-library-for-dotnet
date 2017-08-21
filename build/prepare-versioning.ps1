59ccc3ae70a4c9544d0ef22111a989110ca0e595
REDMOND\kpanwar <kpanwar@microsoft.com>
2017-08-21 13:42:36 -0700

    fix assemblyinfo path

--- build/prepare-versioning.ps1
+++ build/prepare-versioning.ps1
@@ -9,7 +9,7 @@ $hash = git rev-parse HEAD
 
 Write-Host "=========================="
 Write-Host "Versioning assembly info file..."
-$filename = "src\ADAL.Common\CommonAssemblyInfo.cs"
+$filename = "src\Microsoft.IdentityModel.Clients.ActiveDirectory\Properties\AssemblyInfo.cs"
 $content = Get-Content $filename
 $newContent = $content -replace "Microsoft Open Technologies", "Microsoft Corporation"
 $newContent = $newContent + "`n" + "[assembly: AssemblyInformationalVersionAttribute(""$hash"")]"
