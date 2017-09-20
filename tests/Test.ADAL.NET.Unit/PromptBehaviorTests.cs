c89faed4ec51335089822f6aa40e3850b0125234
jennyf19 <ferriesj@gmail.com>
2017-09-19 18:31:38 -0700

    Clean-up tests

--- tests/Test.ADAL.NET.Unit/PromptBehaviorTests.cs
+++ tests/Test.ADAL.NET.Unit/PromptBehaviorTests.cs
@@ -34,22 +34,19 @@ using Test.ADAL.NET.Unit.Mocks;
 using AuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
 using System.Net.Http;
 using Test.ADAL.Common;
+using System.Net;
 
 namespace Test.ADAL.NET.Unit
 {
     [TestClass]
     public class PromptBehaviorTests
     {
-        private PlatformParameters platformParameters;
-
         [TestInitialize]
         public void Initialize()
         {
             HttpMessageHandlerFactory.ClearMockHandlers();
         }
-
-        #region PromptBehavior Tests for each value of prompt behavior
-        // PromptBehavior Auto Test Case
+        
         [TestMethod]
         [Description("Test for Force Prompt with PromptBehavior.Auto")]
         public async Task ForcePromptForAutoPromptBehaviorTestAsync()
@@ -76,8 +73,7 @@ namespace Test.ADAL.NET.Unit
             Assert.AreEqual(TestConstants.DefaultDisplayableId, result.UserInfo.DisplayableId);
             Assert.AreEqual(TestConstants.DefaultUniqueId, result.UserInfo.UniqueId);
         }
-
-        // Test for calling promptBehavior.auto when cache already has an access token
+        
         [TestMethod]
         [Description("Test for calling promptBehavior.Auto when cache already has an access token")]
         public async Task AutoPromptBehaviorWithTokenInCacheTest()
@@ -102,21 +98,65 @@ namespace Test.ADAL.NET.Unit
                         TestConstants.DefaultRedirectUri, new PlatformParameters(PromptBehavior.Auto));
 
             Assert.IsNotNull(result);
-            Assert.AreEqual(TestConstants.DefaultAuthorityHomeTenant, context.Authenticator.Authority);
             Assert.AreEqual("existing-access-token", result.AccessToken);
-            Assert.IsNotNull(result.UserInfo);
 
-            //there should be only one cache entry.
+            // There should be only one cache entry.
             Assert.AreEqual(1, context.TokenCache.Count);
         }
+        
+        [TestMethod]
+        [Description("Test for calling promptBehavior.Auto when cache has an expired access token, but a good refresh token")]
+        public async Task AutoPromptBehaviorWithExpiredAccessTokenAndGoodRefreshTokenInCacheTest()
+        {
+            var context = new AuthenticationContext(TestConstants.DefaultAuthorityHomeTenant, true);
+            context.TokenCache.Clear();
 
-        // PromptBehavior Always Test Case
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler()
+            {
+                Method = HttpMethod.Post,
+                ResponseMessage = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+                {
+                    Content = new StringContent("{\"token_type\":\"Bearer\",\"expires_in\":\"3599\",\"access_token\":\"some-access-token\"}")
+                },
+                PostData = new Dictionary<string, string>()
+                {
+                    {"client_id", TestConstants.DefaultClientId},
+                    {"client_secret", TestConstants.DefaultClientSecret},
+                    {"grant_type", "client_credentials"}
+                }
+            });
+
+            TokenCacheKey key = new TokenCacheKey(TestConstants.DefaultAuthorityHomeTenant,
+                TestConstants.DefaultResource, TestConstants.DefaultClientId, TokenSubjectType.User,
+                TestConstants.DefaultUniqueId, TestConstants.DefaultDisplayableId);
+            context.TokenCache.tokenCacheDictionary[key] = new AuthenticationResultEx
+            {
+                RefreshToken = "some-rt",
+                ResourceInResponse = TestConstants.DefaultResource,
+                Result = new AuthenticationResult("Bearer", "existing-access-token",
+                    DateTimeOffset.UtcNow + TimeSpan.FromMinutes(100))
+            };
+
+            AuthenticationResult result =
+                await
+                    context.AcquireTokenSilentAsync(TestConstants.DefaultResource, TestConstants.DefaultClientId,
+                    new UserIdentifier(TestConstants.DefaultDisplayableId, UserIdentifierType.RequiredDisplayableId),
+                    new PlatformParameters(PromptBehavior.Auto));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("existing-access-token", result.AccessToken);
+
+            // There should be only one cache entry.
+            Assert.AreEqual(1, context.TokenCache.Count);
+        }
+        
         [TestMethod]
         [Description("Test for Force Prompt with PromptBehavior.Always")]
         public async Task ForcePromptForAlwaysPromptBehaviorTestAsync()
         {
             MockHelpers.ConfigureMockWebUI(new AuthorizationResult(AuthorizationStatus.Success,
                 TestConstants.DefaultRedirectUri + "?code=some-code"));
+
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler()
             {
                 Method = HttpMethod.Post,
@@ -128,6 +168,7 @@ namespace Test.ADAL.NET.Unit
             });
 
             var context = new AuthenticationContext(TestConstants.DefaultAuthorityHomeTenant, true);
+            context.TokenCache.Clear();
 
             TokenCacheKey key = new TokenCacheKey(TestConstants.DefaultAuthorityHomeTenant,
                 TestConstants.DefaultResource, TestConstants.DefaultClientId, TokenSubjectType.User,
@@ -145,17 +186,12 @@ namespace Test.ADAL.NET.Unit
                         TestConstants.DefaultRedirectUri, new PlatformParameters(PromptBehavior.Always));
 
             Assert.IsNotNull(result);
-            Assert.AreEqual(TestConstants.DefaultAuthorityHomeTenant, context.Authenticator.Authority);
             Assert.AreEqual("some-access-token", result.AccessToken);
-            Assert.IsNotNull(result.UserInfo);
-            Assert.AreEqual(TestConstants.DefaultDisplayableId, result.UserInfo.DisplayableId);
-            Assert.AreEqual(TestConstants.DefaultUniqueId, result.UserInfo.UniqueId);
 
-            //there should be only one cache entry.
+            // There should be only one cache entry.
             Assert.AreEqual(1, context.TokenCache.Count);
         }
-
-        // PromptBehavior SelectAccount Test Case
+        
         [TestMethod]
         [Description("Test for Force Prompt with PromptBehavior.SelectAccount")]
         public async Task ForcePromptForSelectAccountPromptBehaviorTestAsync()
@@ -201,16 +237,16 @@ namespace Test.ADAL.NET.Unit
             Assert.AreEqual(TestConstants.DefaultDisplayableId, result.UserInfo.DisplayableId);
             Assert.AreEqual(TestConstants.DefaultUniqueId, result.UserInfo.UniqueId);
 
-            //there should be only one cache entry.
+            // There should be only one cache entry.
             Assert.AreEqual(1, context.TokenCache.Count);
         }
-
-        // PromptBehavior Never Test Case
+        
         [TestMethod]
         [Description("Test for Force Prompt with PromptBehavior.Never")]
         public void ForcePromptForNeverPromptBehaviorTestAsync()
         {
             var context = new AuthenticationContext(TestConstants.DefaultAuthorityHomeTenant, new TokenCache());
+            context.TokenCache.Clear();
 
             TokenCacheKey key = new TokenCacheKey(TestConstants.DefaultAuthorityHomeTenant,
                TestConstants.DefaultResource, TestConstants.DefaultClientId, TokenSubjectType.User,
@@ -233,18 +269,17 @@ namespace Test.ADAL.NET.Unit
                         TestConstants.DefaultRedirectUri, new PlatformParameters(PromptBehavior.Never)));
 
             Assert.AreEqual(AdalError.FailedToRefreshToken, exc.ErrorCode);
-            //there should be only one cache entry.
+            // There should be only one cache entry.
             Assert.AreEqual(1, context.TokenCache.Count);
         }
-
-        // PromptBehavior RefreshSession Test Cases
+        
         [TestMethod]
         [Description("Test for Force Prompt with PromptBehavior.RefreshSession")]
         public async Task ForcePromptForRefreshSessionPromptBehaviorTestAsync()
         {
             MockHelpers.ConfigureMockWebUI(new AuthorizationResult(AuthorizationStatus.Success,
                 TestConstants.DefaultRedirectUri + "?code=some-code"),
-                // validate that authorizationUri passed to WebUi contains prompt=select_account query parameter
+                // validate that authorizationUri passed to WebUi contains prompt=refresh_session query parameter
                 new Dictionary<string, string> { { "prompt", "refresh_session" } });
 
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler()
@@ -258,6 +293,7 @@ namespace Test.ADAL.NET.Unit
             });
 
             var context = new AuthenticationContext(TestConstants.DefaultAuthorityHomeTenant, true);
+            context.TokenCache.Clear();
 
             TokenCacheKey key = new TokenCacheKey(TestConstants.DefaultAuthorityHomeTenant,
                 TestConstants.DefaultResource, TestConstants.DefaultClientId, TokenSubjectType.User,
@@ -275,23 +311,10 @@ namespace Test.ADAL.NET.Unit
                         TestConstants.DefaultRedirectUri, new PlatformParameters(PromptBehavior.RefreshSession));
 
             Assert.IsNotNull(result);
-            Assert.AreEqual(TestConstants.DefaultAuthorityHomeTenant, context.Authenticator.Authority);
             Assert.AreEqual("some-access-token", result.AccessToken);
-            Assert.IsNotNull(result.UserInfo);
-            Assert.AreEqual(TestConstants.DefaultDisplayableId, result.UserInfo.DisplayableId);
-            Assert.AreEqual(TestConstants.DefaultUniqueId, result.UserInfo.UniqueId);
 
-            //there should be only one cache entry.
+            // There should be only one cache entry.
             Assert.AreEqual(1, context.TokenCache.Count);
         }
-        #endregion
-
-        #region Prompt behavior specific tests
-
-        // Test for calling promptBehavior.auto when cache already has an expired access token, but good refresh token
-
-        // Test for calling prompBehavior.always when cache already has an access token
-
-        #endregion
     }
 }
