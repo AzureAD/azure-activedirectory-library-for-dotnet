62664d5a17a2774ead3649dd6a6181c346a45a20
jennyf19 <ferriesj@gmail.com>
2017-10-11 12:24:44 -0700

    - In MockHttpMessageHandler, add in constructor and constructor plus overload to specify url endpoint - Add Acquire Token w/empty cache for Federated user test - Change output property for TestMex.xml and WsTrustResponse.xml to Always - Update TextMex.xml from network call - Update WsTrustResponse.xml from network call

--- tests/Test.ADAL.NET.Integration/FederatedUserCredentialTests.cs
+++ tests/Test.ADAL.NET.Integration/FederatedUserCredentialTests.cs
@@ -1,26 +1,22 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Clients.ActiveDirectory.Internal.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Test.ADAL.NET.Common;
 using Test.ADAL.NET.Common.Mocks;
-using Test.ADAL.NET.Unit;
 using AuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
 
 namespace Test.ADAL.NET.Integration
 {
     [TestClass]
+    [DeploymentItem("TestMex.xml")]
     [DeploymentItem("WsTrustResponse.xml")]
     public class FederatedUserCredentialTests
     {
-        [DeploymentItem("WsTrustResponse.xml")]
         [TestInitialize]
         public void Initialize()
         {
@@ -30,8 +26,8 @@ namespace Test.ADAL.NET.Integration
         }
 
         [TestMethod]
-        [Description("Positive Test for AcquireToken with empty cache")]
-        public async Task AcquireTokenWithEmptyCachePositiveTestAsync()
+        [Description("Test for AcquireToken with empty cache")]
+        public async Task AcquireTokenWithEmptyCache_GetsNewTokenFromServiceTestAsync()
         {
             AuthenticationContext context = new AuthenticationContext(TestConstants.DefaultAuthorityCommonTenant, new TokenCache());
             await context.Authenticator.UpdateFromTemplateAsync(null);
@@ -41,7 +37,7 @@ namespace Test.ADAL.NET.Integration
                 Method = HttpMethod.Get,
                 ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
                 {
-                    Content = new StringContent("{\"ver\":\"1.0\",\"account_type\":\"Federated\",\"domain_name\":\"microsoft.com\"," +
+                    Content = new StringContent("{\"ver\":\"1.0\",\"account_type\":\"federated\",\"domain_name\":\"microsoft.com\"," +
                                                 "\"federation_protocol\":\"WSTrust\",\"federation_metadata_url\":" +
                                                 "\"https://msft.sts.microsoft.com/adfs/services/trust/mex\"," +
                                                 "\"federation_active_auth_url\":\"https://msft.sts.microsoft.com/adfs/services/trust/2005/usernamemixed\"" +
@@ -53,15 +49,16 @@ namespace Test.ADAL.NET.Integration
                 }
             });
 
-            UserRealmDiscoveryResponse userRealmResponse = await UserRealmDiscoveryResponse.CreateByDiscoveryAsync(context.Authenticator.UserRealmUri, TestConstants.DefaultDisplayableId, CallState.Default);
-
-            WsTrustAddress address = new WsTrustAddress()
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler("https://msft.sts.microsoft.com/adfs/services/trust/mex")
             {
-                Uri = new Uri("https://some/address/usernamemixed"),
-                Version = WsTrustVersion.WsTrust13
-            };
+                Method = HttpMethod.Get,
+                ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(File.ReadAllText("TestMex.xml"))
+                }
+            });
 
-            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler()
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler("https://msft.sts.microsoft.com/adfs/services/trust/2005/usernamemixed")
             {
                 Method = HttpMethod.Post,
                 ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
@@ -70,9 +67,27 @@ namespace Test.ADAL.NET.Integration
                 }
             });
 
-            WsTrustResponse wsTrustResponse = await WsTrustRequest.SendRequestAsync(address, new UserPasswordCredential(TestConstants.DefaultDisplayableId, TestConstants.DefaultPassword), null, userRealmResponse.CloudAudienceUrn);
-            Assert.IsNotNull(wsTrustResponse.Token);
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler("https://login.microsoftonline.com/common/oauth2/token")
+            {
+                Method = HttpMethod.Post,
+                ResponseMessage = MockHelpers.CreateSuccessTokenResponseMessage(),
+                PostData = new Dictionary<string, string>()
+                {
+                    {"grant_type", "urn:ietf:params:oauth:grant-type:saml1_1-bearer"},
+                    {"scope", "openid"}
+                }
+            });
+
+            // Call acquire token
+            AuthenticationResult result = await context.AcquireTokenAsync(TestConstants.DefaultResource, TestConstants.DefaultClientId,
+                new UserPasswordCredential(TestConstants.DefaultDisplayableId, TestConstants.DefaultPassword));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("some-access-token", result.AccessToken);
+            Assert.AreEqual(TestConstants.DefaultDisplayableId, result.UserInfo.DisplayableId);
+
+            // All mocks are consumed
+            Assert.AreEqual(0, HttpMessageHandlerFactory.MockHandlersCount());
         }
     }
 }
-
