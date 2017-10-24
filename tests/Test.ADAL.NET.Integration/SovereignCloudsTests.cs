25fc5e25cf3513bdebc76d630aaa7e7d4932b52b
jennyf19 <ferriesj@gmail.com>
2017-10-23 19:51:37 -0700

    Add test for making instance discovery all because the authority is not in the instance cache

--- tests/Test.ADAL.NET.Integration/SovereignCloudsTests.cs
+++ tests/Test.ADAL.NET.Integration/SovereignCloudsTests.cs
@@ -38,6 +38,7 @@ using Microsoft.IdentityModel.Clients.ActiveDirectory.Internal.OAuth2;
 using Microsoft.IdentityModel.Clients.ActiveDirectory.Internal;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System.Net;
+using System;
 
 namespace Test.ADAL.NET.Unit
 {
@@ -45,6 +46,10 @@ namespace Test.ADAL.NET.Unit
     public class SovereignCloudsTests
     {
         private PlatformParameters platformParameters;
+
+        const string sovereignAuthorityHost = "login.some-sovereign-cloud";
+
+        string sovereignTenantSpesificAuthority = $"https://{sovereignAuthorityHost}/{TestConstants.SomeTenantId}/";
 
         [TestInitialize]
         public void Initialize()
@@ -59,10 +64,6 @@ namespace Test.ADAL.NET.Unit
         [Description("Sovereign user use world wide authority")]
         public async Task SovereignUserWorldWideAuthorityIntegrationTest()
         {
-            const string sovereignAuthorityHost = "login.some-sovereign-cloud";
-
-            var sovereignTenantSpesificAuthority = $"https://{sovereignAuthorityHost}/{TestConstants.SomeTenantId}/";
-
             // creating AuthenticationContext with common Authority
             var authenticationContext =
                 new AuthenticationContext(TestConstants.DefaultAuthorityCommonTenant, false, new TokenCache());
@@ -74,7 +75,7 @@ namespace Test.ADAL.NET.Unit
             MockHelpers.ConfigureMockWebUI(
                 new AuthorizationResult(AuthorizationStatus.Success, authReturnedUriInputMock),
                 // validate that authorizationUri passed to WebUi contains instance_aware query parameter
-                new Dictionary<string, string> {{"instance_aware", "true"}});
+                new Dictionary<string, string> { { "instance_aware", "true" } });
 
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
             {
@@ -123,7 +124,7 @@ namespace Test.ADAL.NET.Unit
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
             {
                 Method = HttpMethod.Post,
-                ResponseMessage = 
+                ResponseMessage =
                     MockHelpers.CreateSuccessTokenResponseMessage(TestConstants.DefaultUniqueId,
                     TestConstants.DefaultDisplayableId, TestConstants.DefaultResource),
 
@@ -148,6 +149,97 @@ namespace Test.ADAL.NET.Unit
             Assert.AreEqual(1, authenticationContext.TokenCache.tokenCacheDictionary.Count);
             Assert.AreEqual(sovereignTenantSpesificAuthority,
                 authenticationContext.TokenCache.tokenCacheDictionary.Keys.FirstOrDefault().Authority);
+
+            // all mocks are consumed
+            Assert.AreEqual(0, HttpMessageHandlerFactory.MockHandlersCount());
+        }
+
+        [TestMethod]
+        [Description("Instance discovery call is made because authority was not already in the instance cache")]
+        public async Task InstanceDiscoverCallMade_AuthorityNotInInstanceCacheTestAsync()
+        {
+            CallState callState = new CallState(Guid.NewGuid());
+
+            // creating AuthenticationContext with common Authority
+            var authenticationContext =
+                new AuthenticationContext(TestConstants.DefaultAuthorityCommonTenant, false, new TokenCache());
+
+            // mock value for authentication returnedUriInput, with cloud_instance_name claim
+            var authReturnedUriInputMock = TestConstants.DefaultRedirectUri + "?code=some-code" + "&" +
+                                           TokenResponseClaim.CloudInstanceHost + "=" + sovereignAuthorityHost;
+
+            Assert.AreEqual(0, InstanceDiscovery.InstanceCache.Count());
+
+            MockHelpers.ConfigureMockWebUI(
+                new AuthorizationResult(AuthorizationStatus.Success, authReturnedUriInputMock),
+                // validate that authorizationUri passed to WebUi contains instance_aware query parameter
+                new Dictionary<string, string> { { "instance_aware", "true" } });
+
+            // German authority not included
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
+            {
+                Method = HttpMethod.Get,
+                ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(
+                        @"{  
+                            ""tenant_discovery_endpoint"":""https://login.microsoftonline.com/v1/.well-known/openid-configuration"",
+                            ""api-version"":""1.1"",
+                            ""metadata"":[
+                                {
+                                ""preferred_network"":""login.microsoftonline.com"",
+                                ""preferred_cache"":""login.windows.net"",
+                                ""aliases"":[
+                                    ""login.microsoftonline.com"",
+                                    ""login.windows.net"",
+                                    ""login.microsoft.com"",
+                                    ""sts.windows.net""]},
+                                {
+                                ""preferred_network"":""login.microsoftonline.us"",
+                                ""preferred_cache"":""login.microsoftonline.us"",
+                                ""aliases"":[
+                                    ""login.microsoftonline.us"",
+                                    ""login.usgovcloudapi.net""]},
+                                {  
+                                ""preferred_network"":""login -us.microsoftonline.com"",
+                                ""preferred_cache"":""login -us.microsoftonline.com"",
+                                ""aliases"":[
+                                    ""login -us.microsoftonline.com""]}]}"
+                    )
+                }
+            });
+
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
+            {
+                Method = HttpMethod.Post,
+                ResponseMessage =
+                    MockHelpers.CreateSuccessTokenResponseMessage(TestConstants.DefaultUniqueId,
+                    TestConstants.DefaultDisplayableId, TestConstants.DefaultResource),
+
+                AdditionalRequestValidation = request =>
+                {
+                    // make sure that Sovereign authority was used for Authorization request
+                    Assert.AreEqual(sovereignAuthorityHost, request.RequestUri.Authority);
+                }
+            });
+
+            var authenticationResult = await authenticationContext.AcquireTokenAsync(TestConstants.DefaultResource,
+                TestConstants.DefaultClientId,
+                TestConstants.DefaultRedirectUri, platformParameters, UserIdentifier.AnyUser, "instance_aware=true");
+
+            Assert.AreEqual(8, InstanceDiscovery.InstanceCache.Count());
+
+            // German authority not included in instance cache
+            Assert.AreEqual(false, InstanceDiscovery.InstanceCache.Keys.Contains("login.microsoftonline.de"));
+
+            var entry = await InstanceDiscovery.GetMetadataEntry("login.microsoftonline.de", true, callState).ConfigureAwait(false);
+
+            // German authority included in instance caceh
+            Assert.AreEqual("login.microsoftonline.de", entry.PreferredNetwork);
+            Assert.AreEqual(9, InstanceDiscovery.InstanceCache.Count());
+            Assert.AreEqual(true, InstanceDiscovery.InstanceCache.Keys.Contains("login.microsoftonline.de"));
+            Assert.AreEqual(true, InstanceDiscovery.InstanceCache.Keys.Contains("login.windows.net"));
+            Assert.AreEqual(true, InstanceDiscovery.InstanceCache.Keys.Contains("login.some-sovereign-cloud"));
 
             // all mocks are consumed
             Assert.AreEqual(0, HttpMessageHandlerFactory.MockHandlersCount());
