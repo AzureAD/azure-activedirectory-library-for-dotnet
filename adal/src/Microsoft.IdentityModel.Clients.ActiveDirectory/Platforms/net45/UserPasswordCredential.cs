b743e6ef02aa60b317196270b02bcdf5657da09e
Henrik Metzger <henrikm@microsoft.com>
2018-06-01 18:55:33 -0700

    revert secure pwd clear. will be taken care of in change from Ray

--- adal/src/Microsoft.IdentityModel.Clients.ActiveDirectory/Platforms/net45/UserPasswordCredential.cs
+++ adal/src/Microsoft.IdentityModel.Clients.ActiveDirectory/Platforms/net45/UserPasswordCredential.cs
@@ -85,6 +85,13 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
             requestParameters[OAuthParameter.GrantType] = OAuthGrantType.Password;
             requestParameters[OAuthParameter.Username] = this.UserName;
             requestParameters[OAuthParameter.Password] = new string(PasswordToCharArray());
+
+            if (SecurePassword != null && !SecurePassword.IsReadOnly()) 
+            { 
+
+                SecurePassword.Clear(); 
+            } 
+            SecurePassword = null; 
         }
     }
 }
