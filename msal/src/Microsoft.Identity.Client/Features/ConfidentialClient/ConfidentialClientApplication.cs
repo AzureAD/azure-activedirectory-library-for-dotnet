1b40da18506d13e8a69b354b5eda7141e2196620
trwalke <trwalke@microsoft.com>
2018-07-31 16:28:27 -0700

    Updating XML Comments. Updating API surface.

--- msal/src/Microsoft.Identity.Client/Features/ConfidentialClient/ConfidentialClientApplication.cs
+++ msal/src/Microsoft.Identity.Client/Features/ConfidentialClient/ConfidentialClientApplication.cs
@@ -154,38 +154,39 @@ namespace Microsoft.Identity.Client
         }
 
         /// <summary>
-        /// Acquires token from the service for the confidential client. This method attempts to look up valid access token in the cache.
+        /// Acquires token from the service for the confidential client using the client credentials flow. See https://aka.ms/msal-net-client-credentials
         /// </summary>
         /// <param name="scopes">Array of scopes requested for resource</param>
-        /// <param name="sendCert">This parameter enables application developers to achieve easy certificates roll-over
+        /// <param name="sendCertificate">This parameter enables application developers to achieve easy certificates roll-over
         /// in Azure AD: setting this parameter to true will send the public certificate to Azure AD
         /// along with the token request, so that Azure AD can use it to validate the subject name based on a trusted issuer policy.
         /// This saves the application admin from the need to explicitly manage the certificate rollover
         /// (either via portal or powershell/CLI operation)</param>
         /// <returns>Authentication result containing application token for the requested scopes</returns>
-        public async Task<AuthenticationResult> AcquireTokenForClientAsync(bool sendCert, IEnumerable<string> scopes)
+        public async Task<AuthenticationResult> AcquireTokenForClientAsync(bool sendCertificate, IEnumerable<string> scopes)
         {
             return
                 await
-                    AcquireTokenForClientCommonAsync(scopes, false, ApiEvent.ApiIds.AcquireTokenForClientWithScope, sendCert).ConfigureAwait(false);
+                    AcquireTokenForClientCommonAsync(scopes, false, ApiEvent.ApiIds.AcquireTokenForClientWithScope, sendCertificate).ConfigureAwait(false);
         }
 
         /// <summary>
-        /// Acquires token from the service for the confidential client. This method attempts to look up valid access token in the cache.
+        /// Acquires token from the service for the confidential client using the client credentials flow. See https://aka.ms/msal-net-client-credentials.
+        /// This method attempts to look up valid access token in the cache unless <paramref name="forceRefresh"/> is true
         /// </summary>
         /// <param name="scopes">Array of scopes requested for resource</param>
         /// <param name="forceRefresh">If TRUE, API will ignore the access token in the cache and attempt to acquire new access token using client credentials</param>
-        /// <param name="sendCert">This parameter enables application developers to achieve easy certificates roll-over
+        /// <param name="sendCertificate">This parameter enables application developers to achieve easy certificates roll-over
         /// in Azure AD: setting this parameter to true will send the public certificate to Azure AD
         /// along with the token request, so that Azure AD can use it to validate the subject name based on a trusted issuer policy.
         /// This saves the application admin from the need to explicitly manage the certificate rollover
         /// (either via portal or powershell/CLI operation)</param>
         /// <returns>Authentication result containing application token for the requested scopes</returns>
-        public async Task<AuthenticationResult> AcquireTokenForClientAsync(bool sendCert, IEnumerable<string> scopes, bool forceRefresh)
+        public async Task<AuthenticationResult> AcquireTokenForClientAsync(bool sendCertificate, IEnumerable<string> scopes, bool forceRefresh)
         {
             return
                 await
-                    AcquireTokenForClientCommonAsync(scopes, forceRefresh, ApiEvent.ApiIds.AcquireTokenForClientWithScopeRefresh, sendCert).ConfigureAwait(false);
+                    AcquireTokenForClientCommonAsync(scopes, forceRefresh, ApiEvent.ApiIds.AcquireTokenForClientWithScopeRefresh, sendCertificate).ConfigureAwait(false);
         }
 
         /// <summary>
