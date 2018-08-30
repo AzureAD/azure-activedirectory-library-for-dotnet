03f40ddf34121ea387e391efdee4d73a43b34284
Bogdan Gavril <bogavril@microsoft.com>
2018-08-30 17:55:36 +0100

    Common unit tests

--- adal/tests/Test.ADAL.NET.Unit/AdalDotNetTests.cs
+++ adal/tests/Test.ADAL.NET.Unit/AdalDotNetTests.cs
@@ -66,7 +66,7 @@ namespace Test.ADAL.NET.Unit
             InstanceDiscovery.InstanceCache.Clear();
 
             AdalHttpMessageHandlerFactory.AddMockHandler(MockHelpers.CreateInstanceDiscoveryMockHandler(TestConstants.GetDiscoveryEndpoint(TestConstants.DefaultAuthorityCommonTenant)));
-            _platformParameters = new PlatformParameters(Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior.Auto);
+            _platformParameters = new PlatformParameters(PromptBehavior.Auto);
         }
 
         [TestCleanup()]
