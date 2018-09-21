b0dca697438d1fd980ed4180671a022f56271bf2
REDMOND\trwalke <trwalke>
2018-09-21 12:58:02 -0700

    filtering tests for appcenter testing

--- adal/tests/Test.ADAL.NET.UIAutomation/CoreMobileADALTests.cs
+++ adal/tests/Test.ADAL.NET.UIAutomation/CoreMobileADALTests.cs
@@ -28,6 +28,20 @@ namespace Test.ADAL.NET.UIAutomation
 		{
             var user = prepareForAuthentication(controller);
 
+            // Create a string array with the lines of text
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            // Set a variable to the My Documents path.
+            string mydocpath =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath, "WriteLines.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+
             SetInputData(controller, UiAutomationTestClientId, MSGraph);
 
             PerformSignInFlowFlow(controller, user);
