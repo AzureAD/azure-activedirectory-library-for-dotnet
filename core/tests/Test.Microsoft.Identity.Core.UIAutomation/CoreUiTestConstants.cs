203eb02f97085cfa4f50dc630adabb0c78f2ab0c
Jenny Ferries <jeferrie@microsoft.com>
2018-11-01 08:50:35 -0700

    lab test.

--- core/tests/Test.Microsoft.Identity.Core.UIAutomation/CoreUiTestConstants.cs
+++ core/tests/Test.Microsoft.Identity.Core.UIAutomation/CoreUiTestConstants.cs
@@ -9,7 +9,7 @@ namespace Test.Microsoft.Identity.Core.UIAutomation
     public static class CoreUiTestConstants
     {
         //Applications
-        public const string UiAutomationTestClientId = "3c1e0e0d-b742-45ba-a35e-01c664e14b16";
+        public const string UiAutomationTestClientId = "4b0db8c2-9f26-4417-8bde-3f0e3656f8e0"; // clientId for Lab4 which is multi-tenant
         public const string MSIDLAB4ClientId = "4b0db8c2-9f26-4417-8bde-3f0e3656f8e0";
         public const string UIAutomationAppV2 = "1e245a30-49aa-43eb-b9c1-c11b072cc92b";
 
