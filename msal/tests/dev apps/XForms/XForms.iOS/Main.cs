50e207a62696fe1238e2e43f56795199a665b073
sgonzalezMSFT <sagonzal@microsoft.com>
2018-11-19 08:55:09 -0800

    Remove unneeded dependencies

--- msal/tests/dev apps/XForms/XForms.iOS/Main.cs
+++ msal/tests/dev apps/XForms/XForms.iOS/Main.cs
@@ -25,6 +25,10 @@
 //
 //------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
 using UIKit;
 
 namespace XForms.iOS
