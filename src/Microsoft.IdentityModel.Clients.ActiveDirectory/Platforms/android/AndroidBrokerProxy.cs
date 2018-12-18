53aa70070933b833644ff394df4ff7c0d0b13907
REDMOND\trwalke <trwalke>
2018-12-18 11:56:33 -0800

    Adding UID and package name to broker option on anroid to address Silent auth broker issue.

--- src/Microsoft.IdentityModel.Clients.ActiveDirectory/Platforms/android/AndroidBrokerProxy.cs
+++ src/Microsoft.IdentityModel.Clients.ActiveDirectory/Platforms/android/AndroidBrokerProxy.cs
@@ -452,6 +452,9 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory.Internal.Platform
             brokerOptions.PutString(BrokerConstants.AccountExtraQueryParam,
                 request.ExtraQueryParamsAuthentication);
 
+            brokerOptions.PutString(BrokerConstants.CallerInfoPackage, Application.Context.PackageName);
+            brokerOptions.PutString(BrokerConstants.CallerInfoUID, Android.OS.Process.MyPid().ToString(CultureInfo.InvariantCulture));
+
             if (request.Claims != null)
             {
                 brokerOptions.PutString(BrokerConstants.SkipCache, Boolean.TrueString.ToLowerInvariant());
