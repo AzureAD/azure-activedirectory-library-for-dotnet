287fbf2ac50943f3b25a620c2fb2a5a14a2d4cce
trwalke <trwalke@microsoft.com>
2020-01-10 15:46:24 -0800

    Revert "Disabling failing test temporarily"
    This reverts commit 341dad5b7eac8adfe2abb42a594833f175e2fe77.

--- tests/Test.ADAL.NET.Unit.net45/AdalDotNetTests.cs
+++ tests/Test.ADAL.NET.Unit.net45/AdalDotNetTests.cs
@@ -1263,7 +1263,6 @@ namespace Test.ADAL.NET.Unit
 
         [TestMethod]
         [Description("Test for returning entire HttpResponse as inner exception")]
-        [Ignore] //TODO fix broken test
         public void HttpErrorResponseAsInnerException()
         {
             using (var httpManager = new MockHttpManager())
